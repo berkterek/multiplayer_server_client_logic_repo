a3bcf741ab1816a990c2aaa3d94721556220b6db
berkterek <42406263+berkterek@users.noreply.github.com>
2023-11-26 15:55:51 +0300

    feat: sample done

--- ServerSideClientSideLogic/Assets/_Scripts/PlayerController.cs
+++ ServerSideClientSideLogic/Assets/_Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] InputActionReference _move;
+    [SerializeField] InputActionReference _leftButton;
+    [SerializeField] InputActionReference _rightButton;
     [SerializeField] Transform _transform;
     [SerializeField] float _speed;
     [SerializeField] SpriteRenderer _renderer;
     [SerializeField] Color _color;
-    [SerializeField] int _score = 0;
 
     Vector2 _direction;
     Vector3 _movement;
@@ -30,6 +31,8 @@ public class PlayerController : NetworkBehaviour
         _renderer.color = Color.blue;
         
         _move.action.Enable();
+        _leftButton.action.Enable();
+        _rightButton.action.Enable();
 
         _move.action.performed += HandleOnMovement;
         _move.action.canceled += HandleOnMovement;
@@ -39,6 +42,8 @@ public class PlayerController : NetworkBehaviour
     {
         if (!IsOwner) return;
         _move.action.Disable();
+        _leftButton.action.Disable();
+        _rightButton.action.Disable();
 
         _move.action.performed -= HandleOnMovement;
         _move.action.canceled -= HandleOnMovement;
@@ -49,6 +54,17 @@ public class PlayerController : NetworkBehaviour
         if (!IsOwner) return;
         
         _movement = _speed * Time.deltaTime * _direction;
+
+        if (_leftButton.action.WasPressedThisFrame())
+        {
+            //RandomColorChange();
+            RandomColorChangeInsideEveryClientSideServerRpc();
+        }
+
+        if (_rightButton.action.WasPressedThisFrame())
+        {
+            RandomColorChangeAndColorDataSendToServerSideServerRpc();
+        }
     }
 
     void FixedUpdate()
@@ -63,5 +79,47 @@ public class PlayerController : NetworkBehaviour
     void HandleOnMovement(InputAction.CallbackContext context)
     {
         _direction = context.ReadValue<Vector2>();
+    }
+
+    private Color GetRandomColor()
+    {
+        float randomRed = Random.Range(0f, 1f);
+        float randomGreen = Random.Range(0f, 1f);
+        float randomBlue = Random.Range(0f, 1f);
+        
+        Color randomColor = new Color(randomRed, randomGreen, randomBlue);
+        return randomColor;
+    }
+
+    private void RandomColorChange()
+    {
+        _color = GetRandomColor();
+        _renderer.color = _color;
+    }
+
+    [ServerRpc]
+    private void RandomColorChangeInsideEveryClientSideServerRpc()
+    {
+        RandomColorChangeInsideEveryClientSideClientRpc();
+    }
+
+    [ClientRpc]
+    private void RandomColorChangeInsideEveryClientSideClientRpc()
+    {
+        _color = GetRandomColor();
+        _renderer.color = _color;
+    }
+    
+    [ServerRpc]
+    private void RandomColorChangeAndColorDataSendToServerSideServerRpc()
+    {
+        _color = GetRandomColor();
+        RandomColorChangeColorDataSendToClientsSideClientRpc(_color);
+    }
+
+    [ClientRpc]
+    private void RandomColorChangeColorDataSendToClientsSideClientRpc(Color color)
+    {
+        _renderer.color = color;
     }
 }
